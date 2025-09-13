216b925d32ce75030851c9c379e53d2979226a02
Roy <hidiho@gmail.com>
2025-09-13 23:48:42 +0200

    Update test_battery_sessions.py

--- tests/test_battery_sessions.py
+++ tests/test_battery_sessions.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone, date
 
 # Import the function or class you are testing
 from python_frank_energie import FrankEnergie
+from python_frank_energie.models.battery import BatterySession, parse_sessions
 
 def test_parse_smart_battery_sessions(smart_battery_sessions: dict):
     """Validate parsing of smartBatterySessions.json fixture."""
